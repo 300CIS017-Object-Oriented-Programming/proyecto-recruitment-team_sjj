9c9c47d85802291aaca1cc8ae7ce18f3a4e090ea
Juan Felipe Muñoz <jjfmunoz203@javerianacali.edu.co>
2022-10-17 20:36:09 -0500

    New: nuevo cmake para pruebas unitarias y nuevo archivo para las pruebas

--- tst/SistemTest.cpp
+++ tst/SistemTest.cpp
@@ -1,3 +1,10 @@
 //
 // Created by Pipe on 17/10/2022.
 //
+#include "gtest/gtest.h"
+#include "../src/Sistema.cpp"
+
+
+TEST(){
+
+}
---
+++ tst/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst
+# Build directory: C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst/cmake-build-debug/Project_tst[1]_include.cmake")
+add_test(Project_tst "C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst/cmake-build-debug/Project_tst.exe")
+set_tests_properties(Project_tst PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst/CMakeLists.txt;11;add_test;C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst/CMakeLists.txt;0;")
