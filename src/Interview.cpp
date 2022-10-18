6af7a226e89b5859ef74fcd1033d8235b4d52b77
Sebastian Dow <94850342+Sebastian-Dow@users.noreply.github.com>
2022-10-17 21:52:14 -0500

    Add files via upload

---
+++ src/Interview.cpp
@@ -0,0 +1,18 @@
+//
+// Created by JOSUE DAVID on 28/09/2022.
+//
+
+#include "Interview.h"
+#include <iostream>
+
+Interview::Interview() {
+    hired = false;
+}
+
+bool Interview::getHired() const{
+    return hired;
+}
+
+void Interview::setHired(bool hired) {
+    Interview::hired= hired;
+}
