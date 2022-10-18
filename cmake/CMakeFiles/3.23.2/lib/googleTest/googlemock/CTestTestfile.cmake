d30de76e99801ac3e63669a157d4d18bafa5dea7
Josueruano <110186574+Josueruano@users.noreply.github.com>
2022-10-17 23:17:04 -0500

    Delete cmake/CMakeFiles/3.23.2 directory

--- cmake/CMakeFiles/3.23.2/lib/googleTest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/lib/googleTest/googlemock
-# Build directory: C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/cmake-build-debug/lib/googleTest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake/CMakeFiles/3.23.2/tst/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst
-# Build directory: C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/cmake-build-debug/tst
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/cmake-build-debug/tst/RecruiterSoftware_tst[1]_include.cmake")
-add_test([=[RecruiterSoftware_tst]=] "C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/cmake-build-debug/tst/RecruiterSoftware_tst.exe")
-set_tests_properties([=[RecruiterSoftware_tst]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst/CMakeLists.txt;13;add_test;C:/Users/jfmun/github curso/recruiterSoftware/proyecto-recruitment-team_sjj/tst/CMakeLists.txt;0;")
